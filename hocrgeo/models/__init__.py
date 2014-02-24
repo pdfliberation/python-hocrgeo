9036cdc651f497e72383c597d23e19ec46095c0d
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-02-24 00:04:40 -0500

    Okay, the model is more than we need. Parse it all into basic python objects, which is already easy to serialize.

--- hocrgeo/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from hocrgeo.models.hocr import HOCRDocument
--- hocrgeo/parsers/hocr.py
+++ hocrgeo/parsers/hocr.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup
 import re
 
-from hocrgeo.models.hocr import HOCRDocument
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,12 +9,6 @@ logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
-
-PAGE = 'ocr_page'
-CAREA = 'ocr_carea'
-PAR = 'ocr_par'
-LINE = 'ocr_line'
-WORD = 'ocrx_word'
 
 class HOCRParser:
     """
@@ -54,10 +46,35 @@ class HOCRParser:
 
     @property
     def document(self):
+        '''Parsed HOCR document'''
         return self._doc
 
     def parse(self, inputfile=None):
         '''Parse hOCR document into a python object.'''
+
+        def _extract_objects_from_element(root, el_name, el_class):
+            nodes = root.find_all(el_name, el_class)
+            objects = []
+            for n in nodes:
+                obj = _extract_features(n)
+                objects.append(obj)
+
+            return (nodes, objects)
+
+        def _extract_bbox(input_str):
+            '''Regular expression matching on a input_str that should contain hOCR bbox coordinates.'''
+            match = self._bboxreg.search(input_str)
+            if match:
+                return match.groupdict()
+            return None
+
+        def _extract_features(element):
+            '''Extract basic hOCR features from a given element.'''
+            features = {}
+            features['id'] = element.get('id')
+            features['bbox'] = _extract_bbox(element.get('title', ''))
+            return features
+
         if inputfile:
             self.load_file(inputfile)
         elif not self._rawdata:
@@ -65,46 +82,51 @@ class HOCRParser:
 
         soup = BeautifulSoup(self._rawdata, "lxml")
 
-        self._doc = HOCRDocument()
+        self._doc = {}
 
         # Extract ocr system metadata
         ocr_system = soup.find('meta', attrs={'name': 'ocr-system'})
-        self._doc.add('ocr-system', ocr_system.get('content', None) if ocr_system else None)
+        self._doc['ocr-system'] = ocr_system.get('content', None) if ocr_system else None
 
         # Extract capabilities
         ocr_capabilities = soup.find('meta', attrs={'name': 'ocr-capabilities'})
-        self._doc.add('ocr-capabilities', ocr_capabilities.get('content', ' ').split(' '))
+        self._doc['ocr-capabilities'] = ocr_capabilities.get('content', ' ').split(' ')
 
-        all_pages = soup.find_all('div', PAGE)
-        logger.info('Found {0} page(s)'.format(len(all_pages)))
+        page_nodes, page_objects = _extract_objects_from_element(soup, 'div', 'ocr_page')
+        page_tup = zip(page_nodes, page_objects)
+        logger.info('Found {0} page(s)'.format(len(page_tup)))
 
-        for page in all_pages:
-            page_obj = self._extract_features(page)
-            # page_careas = page.find_all('div', CAREA)
-            logger.info('Adding a page')
-            self._doc.add_page(page_obj)
+        for page_node, page_obj in page_tup:
+            carea_nodes, carea_objects = _extract_objects_from_element(page_node, 'div', 'ocr_carea')
+            careas_tup = zip(carea_nodes, carea_objects)
 
-        # all_careas = soup.find_all('div', CAREA)
-        # logger.info('Found {0} carea(s)'.format(len(all_careas)))
+            for c_node, c_obj in careas_tup:
+                para_nodes, para_objects = _extract_objects_from_element(c_node, 'p', 'ocr_par')
+                paras_tup = zip(para_nodes, para_objects)
 
-        # for carea in all_careas:
-        #     carea_obj = self._extract_features(carea)
-        #     logger.info('Adding a carea: {0}'.format(carea_obj))
-        #     # page_obj['careas'].append(carea_obj)
-        #     parent_obj = carea.parent.get('id', None)
-        #     logger.info(parent_obj)
+                for para_node, para_obj in paras_tup:
+                    line_nodes, line_objects = _extract_objects_from_element(para_node, 'span', 'ocr_line')
+                    lines_tup = zip(line_nodes, line_objects)
 
-    def _extract_bbox(self, input_str):
-        '''Regular expression matching on a input_str that should contain hOCR bbox coordinates.'''
-        match = self._bboxreg.search(input_str)
-        if match:
-            return match.groupdict()
-        return None
+                    for l_node, l_obj in lines_tup:
+                        word_nodes, word_objects = _extract_objects_from_element(l_node, 'span', 'ocrx_word')
+                        words_tup = zip(word_nodes, word_objects)
 
-    def _extract_features(self, element):
-        '''Extract basic hOCR features from a given element.'''
-        features = {}
-        features['id'] = element.get('id')
-        features['bbox'] = self._extract_bbox(element.get('title', ''))
-        return features
+                        for w_node, w_obj in words_tup:
+                            word_str = w_node.get_text(strip=True)
+                            if word_str:
+                                logger.info(word_str)
+                                w_obj['text'] = w_node.get_text()
+                        l_obj['words'] = word_objects
+
+                    para_obj['lines'] = line_objects
+
+                c_obj['paragraphs'] = para_objects
+
+            page_obj['careas'] = carea_objects
+
+        self._doc['pages'] = page_objects
+
+
+
 
