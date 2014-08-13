ca73649a4939fd54ceaf8df4f8fd30da36402d98
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-08-13 16:19:59 -0400

    Package up hocrgeo

--- hocrgeo/__init__.py
+++ hocrgeo/__init__.py
@@ -4,3 +4,6 @@
 
 __appname__ = "hocrgeo"
 __version__ = "0.0.1"
+__author__ = 'Daniel Cloud'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2014 Daniel Cloud'
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+
+import hocrgeo
+
+setup(name='hocrgeo',
+      version=hocrgeo.__version__,
+      description='Python tool for converting hOCR files to geographic file formats',
+      author='Daniel Cloud',
+      author_email='daniel@danielcloud.org',
+      license = hocrgeo.__license__,
+      url='https://github.com/pdfliberation/python-hocrgeo',
+      install_requires=['Shapely', 'lxml', 'beautifulsoup4'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
